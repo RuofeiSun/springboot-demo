28eee28d0e36882786da3ffb758d2ded00d1233c
2587 <sunruofei95>
2021-01-26 20:47:14 +0800

    BUG

--- springboot-security/src/main/java/com/ruofei/component/RestfulAccessDeniedHandler.java
+++ springboot-security/src/main/java/com/ruofei/component/RestfulAccessDeniedHandler.java
@@ -1,6 +1,7 @@
 package com.ruofei.component;
 
 import cn.hutool.json.JSONUtil;
+import com.ruofei.common.CommonResult;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -22,7 +23,7 @@ public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
         response.setHeader("Cache-Control","no-cache");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
-        response.getWriter().println(JSONUtil.parse(e.getMessage()));
+        response.getWriter().println(JSONUtil.parse(CommonResult.forbidden(e.getMessage())));
         response.getWriter().flush();
     }
 }
